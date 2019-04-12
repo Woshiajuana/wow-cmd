6f62e9e949a4deb4a54b8481f461ebcd3306f172
Woshiajuana <zhigang.chen@owulia.com>
2019-04-12 17:09:13 +0800

    提交更新页面

--- utils/task.util.js
+++ utils/task.util.js
@@ -4,17 +4,21 @@ module.exports = {
         let index = 0;
         let len = taskArr.length;
         let next = (data) => {
-            if(index >= len) return;
+            if(index >= len)
+                return null;
             ((i) => {
                 let {
                     fireFun,
                     options,
                 } = taskArr[i];
-                fireFun(options, data).then((res) => {
-                    fireFun.success && fireFun.success(res, next);
-                }).catch((err) => {
-                    fireFun.error && fireFun.error(err, next);
-                });
+                let fn = fireFun(options, data, next);
+                if (fn instanceof Promise) {
+                    fireFun(options, data, next).then((res) => {
+                        fireFun.success && fireFun.success(res, next);
+                    }).catch((err) => {
+                        fireFun.error && fireFun.error(err, next);
+                    });
+                }
             })(index);
             index++;
         };
